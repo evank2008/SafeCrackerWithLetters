bba8f8ec98afd80f83b04fcd3fb7ce64e0dc408d
omega <omega@GAMER-PC>
2023-04-27 18:02:42 -0700

    expanded it to 4 digits and wrote documentation for how to expand it further.

--- Safecracker with Letters/src/_02_methods/_2_safe_cracker_with_letters/SafeCracker.java
+++ Safecracker with Letters/src/_02_methods/_2_safe_cracker_with_letters/SafeCracker.java
@@ -1,7 +1,19 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+/*
+ HOW TO ADD ANOTHER LETTER
+ 1. Add another string under the SafeCracker class. Name it l
+  and then the next number after the previous one.
+ 2.In the cracker() void, copy from the word 'for' at the top to the bracket right after the comment,
+ "This bracket is for the switch".
+ 3. Paste above the previous for loop.
+ 4. In the switch, MANUALLY :( change EACH :( instance of the l variable and the dig variable to the next number up.
+ :(
+ 5. At the bottom of the cracker() void, find the line that runs the tryCode() void. Add in the new l int.
+ 6. Add a new bracket at the end.
+ 
+ */
 package _02_methods._2_safe_cracker_with_letters;
 
 import java.io.File;
@@ -13,6 +25,10 @@ import javax.swing.JOptionPane;
 public class SafeCracker {
 String l1 = null;
 String l2 = null;
+String l3 = null;
+String l4 = null;
+
+
 	/***********  SOUND ***************
 	 * Some computers are unable to play sounds. 
 	 * If you cannot play sound on this computer, set canPlaySounds to false.
@@ -37,7 +53,182 @@ String l2 = null;
 
 	void cracker() {
 		System.out.println("cracker ran");
-			for(int dig2 = 0;dig2<=26;) {
+		for(int dig4 = 0;dig4<=26;) {
+			switch (dig4) {
+			case 0:
+				l4="";
+				break;
+			case 1:
+				l4 = "a";
+				break;
+			case 2:
+				l4 = "b";
+				break;
+			case 3:
+				l4 = "c";
+				break;
+			case 4:
+				l4 = "d";
+				break;
+			case 5:
+				l4 = "e";
+				break;
+			case 6:
+				l4 = "f";
+				break;
+			case 7:
+				l4 = "g";
+				break;
+			case 8:
+				l4 = "h";
+				break;
+			case 9:
+				l4 = "i";
+				break;
+			case 10:
+				l4 = "j";
+				break;
+			case 11:
+				l4 = "k";
+				break;
+			case 12:
+				l4 = "l";
+				break;
+			case 13:
+				l4 = "m";
+				break;
+			case 14:
+				l4 = "n";
+				break;
+			case 15:
+				l4 = "o";
+				break;
+			case 16:
+				l4 = "p";
+				break;
+			case 17:
+				l4 = "q";
+				break;
+			case 18:
+				l4 = "r";
+				break;
+			case 19:
+				l4 = "s";
+				break;
+			case 20:
+				l4 = "t";
+				break;
+			case 21:
+				l4 = "u";
+				break;
+			case 22:
+				l4 = "v";
+				break;
+			case 23:
+				l4 = "w";
+				break;
+			case 24:
+				l4 = "x";
+				break;
+			case 25:
+				l4 = "y";
+				break;
+			case 26:
+				l4 = "z";
+				break;
+			default:
+				System.out.println("error");
+				//this bracket is for the switch
+			} 
+		for(int dig3 = 0;dig3<=26;) {
+			switch (dig3) {
+			case 0:
+				l3="";
+				break;
+			case 1:
+				l3 = "a";
+				break;
+			case 2:
+				l3 = "b";
+				break;
+			case 3:
+				l3 = "c";
+				break;
+			case 4:
+				l3 = "d";
+				break;
+			case 5:
+				l3 = "e";
+				break;
+			case 6:
+				l3 = "f";
+				break;
+			case 7:
+				l3 = "g";
+				break;
+			case 8:
+				l3 = "h";
+				break;
+			case 9:
+				l3 = "i";
+				break;
+			case 10:
+				l3 = "j";
+				break;
+			case 11:
+				l3 = "k";
+				break;
+			case 12:
+				l3 = "l";
+				break;
+			case 13:
+				l3 = "m";
+				break;
+			case 14:
+				l3 = "n";
+				break;
+			case 15:
+				l3 = "o";
+				break;
+			case 16:
+				l3 = "p";
+				break;
+			case 17:
+				l3 = "q";
+				break;
+			case 18:
+				l3 = "r";
+				break;
+			case 19:
+				l3 = "s";
+				break;
+			case 20:
+				l3 = "t";
+				break;
+			case 21:
+				l3 = "u";
+				break;
+			case 22:
+				l3 = "v";
+				break;
+			case 23:
+				l3 = "w";
+				break;
+			case 24:
+				l3 = "x";
+				break;
+			case 25:
+				l3 = "y";
+				break;
+			case 26:
+				l3 = "z";
+				break;
+			default:
+				System.out.println("error");
+				//this bracket is for the switch
+			}
+			
+		for(int dig2 = 0;dig2<=26;) {
 			switch (dig2) {
 			case 0:
 				l2="";
@@ -124,6 +315,7 @@ String l2 = null;
 				System.out.println("error");
 				//this bracket is for the switch
 			}
+			
 			for(int dig1 = 1;dig1<=26;) {
 				switch (dig1) {
 				case 1:
@@ -208,20 +400,26 @@ String l2 = null;
 					System.out.println("error");
 					//this bracket is for the switch
 				}
-				tryCode(l2+l1);
+				tryCode(l4+l3+l2+l1);
 				dig1++;
 				}
-		dig2++;
+			//this bracket is for digit 1
+			dig2++;
 		//this bracket is for the for loop
 			}
-			System.out.println("No valid codes with a length of two letters. Trying three letters.");
-	//this bracket is for the void oneLetter
+		dig3++;
 		}
+	dig4++;
+		}
+	}
+	
+	//this bracket is for the void oneLetter
+		
 	
 	 void tryCode(String guess) {
 		System.out.println("trying " + guess);
 
-		String secretCode = "zh";
+		String secretCode = "zhaf";
 
 		if (guess.equals(secretCode)) {
 			JOptionPane.showMessageDialog(null, "Congratulations! You cracked the safe with " + guess);
